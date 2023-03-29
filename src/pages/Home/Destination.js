423e056939c40cdc96e5d7d772a40982e38227d7
ehasanulh1 <ehasanulh1@gmail.com>
2023-03-29 12:15:10 +0600

    services card done

--- src/pages/Home/Destination.js
+++ src/pages/Home/Destination.js
@@ -7,14 +7,14 @@ const Destination = () => {
     const destinationData = useLoaderData()
     console.log(destinationData)
     return (
-        <div className='py-28'>
-            <div className='container-xl mx-auto px-4'>
+        <div style={{ maxWidth: '1200px' }} className='mx-auto px-4 py-28'>
+            <div>
                 <div className='text-center pb-20'>
                     <h1 className='font-sans font-bold text-neutral-900 text-4xl uppercase mb-8'>POPULAR DESTINATIONS</h1>
                     <p className='text-lg italic mb-8 tracking-widest font-mono'>Here you can visit some beautiful places, we will help you with all the facilities.</p>
                     <img className='mx-auto text-yellow-500' src={line} alt="" />
                 </div>
-                <div className='grid grid-cols-1 lg:grid-cols-3 gap-5'>
+                <div className='grid grid-cols-1 lg:grid-cols-3 gap-8'>
                     {
                         destinationData.map(destination => <DestinationCard
                             key={destination._id}
--- src/pages/Home/DestinationCard.js
+++ src/pages/Home/DestinationCard.js
@@ -1,9 +1,64 @@
 import React from 'react';
+import { FaStar } from 'react-icons/fa';
 
-const DestinationCard = () => {
+const DestinationCard = ({ destination }) => {
+    const { img, title, price, rating, description
+    } = destination;
     return (
-        <div>
-            destination card
+        <div className="card border-b border-yellow-500 bg-white rounded-none hover:drop-shadow-xl">
+            <figure>
+                <img src={img} alt="Shoes" />
+            </figure>
+            <div className="card-body p-5 items-center text-center">
+                <h2 className="card-title font-bold font-sans text-2xl uppercase tracking-wider">{title}</h2>
+
+                <div className='flex text-xs my-3'>
+                    {
+                        rating === 5 ?
+                            <>
+                                <FaStar className='text-yellow-500 mr-0.5' />
+                                <FaStar className='text-yellow-500 mr-0.5' />
+                                <FaStar className='text-yellow-500 mr-0.5' />
+                                <FaStar className='text-yellow-500 mr-0.5' />
+                                <FaStar className='text-yellow-500 mr-0.5' />
+                            </>
+                            :
+                            <></>
+                    }
+                    {
+                        rating === 4 ?
+                            <>
+                                <FaStar className='text-yellow-500 mr-0.5' />
+                                <FaStar className='text-yellow-500 mr-0.5' />
+                                <FaStar className='text-yellow-500 mr-0.5' />
+                                <FaStar className='text-yellow-500 mr-0.5' />
+                                <FaStar className=' me-1' />
+                            </>
+                            :
+                            <></>
+                    }
+                    {
+                        rating === 3 ?
+                            <>
+                                <FaStar className='text-yellow-500 mr-0.5' />
+                                <FaStar className='text-yellow-500 mr-0.5' />
+                                <FaStar className='text-yellow-500 mr-0.5' />
+                                <FaStar className='me-1' />
+                                <FaStar className='me-1' />
+                            </>
+                            :
+                            <></>
+                    }
+
+                </div>
+
+                <p className='p-0 m-0 font-semibold font-sans text-sm uppercase tracking-wider'>Price: ${price}.00
+                </p>
+
+                <div className="card-actions my-3">
+                    <button className="px-9 py-4 uppercase font-bold text-sm tracking-widest text-yellow-500 rounded-[3px] border border-yellow-500 hover:text-white hover:bg-yellow-500">Book Now</button>
+                </div>
+            </div>
         </div>
     );
 };
